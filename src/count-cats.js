673ad19e0d30f56d8d58bc713d673577b8ecc4eb
abr-ya <gamer05@list.ru>
2021-03-09 12:51:57 +0300

    ref: Update cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,3 @@
-const CustomError = require("../extensions/custom-error");
-
 const countIt = (arr, str) => arr.reduce((sum, it) => sum + (it === str ? 1 : 0), 0);
-
-module.exports = function countCats(arr) {
-  return arr.reduce((sum, it) => sum + countIt(it, '^^'), 0);
-};
+const countCats = (arr) => arr.reduce((sum, it) => sum + countIt(it, '^^'), 0);
+module.exports = countCats;
